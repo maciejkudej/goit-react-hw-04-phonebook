ea8f39d2c6699d6ac5ab2d5f4a8428cba9c41947
maciejkudej <maciejkudej765@gmail.com>
2023-07-03 15:44:46 +0200

    Add useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,23 @@ import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 import css from './App.module.css';
 
+const defaultItems = [
+  { id: shortid.generate(), name: 'Rosie Simpson', number: '459-12-56' },
+  { id: shortid.generate(), name: 'Hermione Kline', number: '443-89-12' },
+  { id: shortid.generate(), name: 'Eden Clements', number: '645-17-79' },
+  { id: shortid.generate(), name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export default function App() {
   const [contacts, setContacts] = useState(() => {
-    return (
-      JSON.parse(window.localStorage.getItem('contacts')) ?? [
-        { id: shortid.generate(), name: 'Rosie Simpson', number: '459-12-56' },
-        { id: shortid.generate(), name: 'Hermione Kline', number: '443-89-12' },
-        { id: shortid.generate(), name: 'Eden Clements', number: '645-17-79' },
-        { id: shortid.generate(), name: 'Annie Copeland', number: '227-91-26' },
-      ]
-    );
+    return JSON.parse(window.localStorage.getItem('contacts') ?? defaultItems);
   });
+
+  useEffect(() => {
+    setContacts(
+      JSON.parse(window.localStorage.getItem('contacts') ?? defaultItems)
+    );
+  }, []);
 
   const [filter, setFilter] = useState('');
 
